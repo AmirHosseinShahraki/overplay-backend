27993dd3e5c641de10edbb6bc285ef6b2313de13
Amirhossein Shahraki <amirhossein.shahraki@protonmail.com>
2024-07-19 15:22:41 +0330

    init: project development preparation

---
+++ src/Api/Program.cs
@@ -0,0 +1,14 @@
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+WebApplication app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.Run();
